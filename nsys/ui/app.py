9cdbbb8b17d35ff8b7f6be1683efca6f4c9215db
Raghav67816 <kumaraghav079@gmail.com>
2026-03-15 15:42:22 +0530

    adding submodule

--- nsys/ui/app.py
+++ nsys/ui/app.py
@@ -15,9 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QGroupBox, QHBoxLayout,
-    QLabel, QMainWindow, QSizePolicy, QSplitter,
-    QToolButton, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QFrame,
+    QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+    QMainWindow, QPushButton, QSizePolicy, QSlider,
+    QSplitter, QToolButton, QVBoxLayout, QWidget)
 from ui import resources_rc
 
 class Ui_AppWindow(object):
@@ -46,15 +47,22 @@ class Ui_AppWindow(object):
         self.topBar.setMaximumSize(QSize(16777215, 40))
         self.topBar.setFrameShape(QFrame.Shape.StyledPanel)
         self.topBar.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.topBar)
+        self.horizontalLayout_4.setSpacing(0)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(12, 0, 12, 0)
         self.boardBtn = QToolButton(self.topBar)
         self.boardBtn.setObjectName(u"boardBtn")
-        self.boardBtn.setGeometry(QRect(10, 10, 26, 25))
+        self.boardBtn.setMinimumSize(QSize(40, 40))
         icon = QIcon()
         icon.addFile(u":/icons/connection.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         self.boardBtn.setIcon(icon)
         self.boardBtn.setIconSize(QSize(20, 20))
 
-        self.splitterLayout.addWidget(self.topBar)
+        self.horizontalLayout_4.addWidget(self.boardBtn)
+
+
+        self.splitterLayout.addWidget(self.topBar, 0, Qt.AlignmentFlag.AlignLeft)
 
         self.splitter = QSplitter(self.container)
         self.splitter.setObjectName(u"splitter")
@@ -87,6 +95,45 @@ class Ui_AppWindow(object):
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.ds_box = QGroupBox(self.right_container)
         self.ds_box.setObjectName(u"ds_box")
+        self.verticalLayout_3 = QVBoxLayout(self.ds_box)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.deviceSelectionBox = QComboBox(self.ds_box)
+        self.deviceSelectionBox.addItem("")
+        self.deviceSelectionBox.setObjectName(u"deviceSelectionBox")
+
+        self.verticalLayout_3.addWidget(self.deviceSelectionBox)
+
+        self.dsParamLayout = QFormLayout()
+        self.dsParamLayout.setObjectName(u"dsParamLayout")
+        self.freqLabel = QLabel(self.ds_box)
+        self.freqLabel.setObjectName(u"freqLabel")
+
+        self.dsParamLayout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.freqLabel)
+
+        self.freqInput = QLineEdit(self.ds_box)
+        self.freqInput.setObjectName(u"freqInput")
+
+        self.dsParamLayout.setWidget(0, QFormLayout.ItemRole.FieldRole, self.freqInput)
+
+        self.noiseLabel = QLabel(self.ds_box)
+        self.noiseLabel.setObjectName(u"noiseLabel")
+
+        self.dsParamLayout.setWidget(1, QFormLayout.ItemRole.LabelRole, self.noiseLabel)
+
+        self.noiseLevel = QSlider(self.ds_box)
+        self.noiseLevel.setObjectName(u"noiseLevel")
+        self.noiseLevel.setOrientation(Qt.Orientation.Horizontal)
+
+        self.dsParamLayout.setWidget(1, QFormLayout.ItemRole.FieldRole, self.noiseLevel)
+
+
+        self.verticalLayout_3.addLayout(self.dsParamLayout)
+
+        self.toggleDataBtn = QPushButton(self.ds_box)
+        self.toggleDataBtn.setObjectName(u"toggleDataBtn")
+
+        self.verticalLayout_3.addWidget(self.toggleDataBtn)
+
 
         self.verticalLayout_2.addWidget(self.ds_box)
 
@@ -149,6 +196,11 @@ class Ui_AppWindow(object):
         AppWindow.setWindowTitle(QCoreApplication.translate("AppWindow", u"NID-24 Analysis Software", None))
         self.boardBtn.setText("")
         self.ds_box.setTitle(QCoreApplication.translate("AppWindow", u"Data Simulator", None))
+        self.deviceSelectionBox.setItemText(0, QCoreApplication.translate("AppWindow", u"Simulated", None))
+
+        self.freqLabel.setText(QCoreApplication.translate("AppWindow", u"Frequency:", None))
+        self.noiseLabel.setText(QCoreApplication.translate("AppWindow", u"Noise Level:", None))
+        self.toggleDataBtn.setText(QCoreApplication.translate("AppWindow", u"Start", None))
         self.featuresBox.setTitle(QCoreApplication.translate("AppWindow", u"Features", None))
         self.statusText.setText(QCoreApplication.translate("AppWindow", u"Status:", None))
         self.statusVal.setText(QCoreApplication.translate("AppWindow", u"Disconnected", None))
