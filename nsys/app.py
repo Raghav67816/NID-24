acbffacab081145e03bcf96be8024bf09f7ec802
Raghav67816 <kumaraghav079@gmail.com>
2026-03-15 21:24:30 +0530

    feature testing - bluetooth integration

--- nsys/app.py
+++ nsys/app.py
@@ -1,10 +1,10 @@
 # import dependencies
 from ui.app import Ui_AppWindow
 
-from PySide6.QtCore import QMargins, Qt
 from PySide6.QtGui import QAction
 from theme_engine import prepare_sheet 
-from PySide6.QtWidgets import QApplication, QMainWindow, QMenu
+from PySide6.QtCore import QMargins, Qt, QProcess, QIODevice
+from PySide6.QtWidgets import QApplication, QMainWindow, QMenu, QMessageBox
 
 import pyqtgraph as pg
 
@@ -23,6 +23,8 @@ class AppWindow(QMainWindow):
 
         # define utilities here
         self.board_manager = BoardManager()
+        self.board_manager.on_rfcomm_started.connect(self.on_rfcomm_started)
+        self.board_manager.on_rfcomm_finished.connect(self.on_rfcomm_finished)
 
         self.ui.boardBtn.clicked.connect(self.board_manager.show_board_manager)
         self.channels = prepare_graphs(self.ui.graphLayout)
@@ -34,6 +36,27 @@ class AppWindow(QMainWindow):
 
     def contextMenuEvent(self, event):
         self.menu.exec(event.globalPos())
+
+    """
+    RF COMM CALLBACKS
+    """
+    def on_rfcomm_started(self):
+        self.ui.statusVal.setText("CONNECTED")
+        self.board_manager.serial_port.open(QIODeviceBase.OpenModeFlag.ReadOnly)
+
+    def on_rfcomm_finished(self, code: int):
+        self.ui.statusVal.setText("DISCONNECTED")
+        if code != 0:
+            msgBox = QMessageBox()
+            msgBox.setWindowTitle("Connection Error")
+            msgBox.setText("RFCOMM process exited with non 0 exit code.")
+            msgBox.setStandardButtons(QMessageBox.StandardButton.Close)
+            msgBox.show()
+            msgBox.exec()
+
+    def read_serial_data(self):
+        while self.board_manager.serial_port.canReadLine():
+            self.process_data(self.board_manager.serial_port.readLine().data.decode('utf-8').strip())            
 
 app = QApplication()
 app_win = AppWindow()
--- nsys/board_manager.py
+++ nsys/board_manager.py
@@ -1,9 +1,10 @@
 from theme_engine import prepare_sheet
 from ui.connection_ui import Ui_DeviceConnDialog
 
-from PySide6.QtWidgets import QDialog, QListWidgetItem
-from PySide6.QtCore import QObject, Signal
+from PySide6.QtSerialPort import QSerialPort
+from PySide6.QtCore import QObject, Signal, QProcess
 from PySide6.QtBluetooth import QBluetoothLocalDevice
+from PySide6.QtWidgets import QDialog, QListWidgetItem
 
 
 """
@@ -25,9 +26,21 @@ class BoardManager(QDialog):
 
         self.localDevice = QBluetoothLocalDevice()
         self.deviceItems = []
+        self.rfcomm_process = None
+
+        self.serial_port = QSerialPort()
+        # configure serial port
+        self.serial_port.setPortName("/dev/rfcomm0")
+        self.serial_port.setReadBufferSize(1024)
+        self.serial_port.setBaudRate(QSerialPort.Baud11520) # check this
+
+        self.on_device_selected = Signal(str)
+        self.on_rfcomm_started = Signal(str)
+        self.on_rfcomm_finished = Signal(int)
 
         self.refresh_view()
 
+        self.on_device_selected.connect(__on_device_selected__)
         self.ui.refreshBtn.clicked.connect(self.refresh_view)
 
     def refresh_view(self):
@@ -48,3 +61,21 @@ class BoardManager(QDialog):
     def show_board_manager(self):
         self.show()
         self.exec()
+
+
+    def on_dialog_accepted(self):
+        device = self.ui.devicesContainer.currentItem()
+        self.on_device_selected.emit(device.text())
+
+    def __on_device_selected__(self, device_addr: str):
+        self.rfcomm_process = QProcess()
+        self.rfcomm_process.setArguments(["bind", "/dev/rfcomm0", device_addr, 1])
+        self.rfcomm_process.started.connect(self.__on_rfcomm_started__)
+        self.rfcomm_process.finished.connect(self.__on_rfcomm_exit__)
+        self.rfcomm_process.execute()
+
+    def __on_rfcomm_exit__(self, code: int, exitStatus: QProcess.ExitStatus):
+        self.on_rfcomm_exit.emit(code)
+
+    def __on_rfcomm_started__(self):
+        self.on_rfcomm_started.emit()
